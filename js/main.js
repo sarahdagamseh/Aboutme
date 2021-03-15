91e1976bc267a88d33fc1dd17a67dc209bbc56da
sarah dagamseh <dagamsehsarah@gmail.com>
2021-03-15 16:51:24 +0200

    lab02

---
+++ js/main.js
@@ -0,0 +1,55 @@
+'use strict'
+
+let whatName =prompt('Do you know my name ?');
+
+if(whatName.toLowerCase() === 'yes')
+{
+  alert('thank you');
+}else if(whatName.toLowerCase() === 'no')
+{
+  alert('My name is Sarah Dagamseh');
+}
+
+let age =prompt('Do you know how old I am ?');
+
+if(age.toUpperCase() === 'YES')
+{
+  alert('Great!!');
+}
+else if(age.toUpperCase() === 'NO')
+{
+  alert ('Its 31');
+}
+let uni =prompt('Do you know the name of university i graduated from?');
+
+if(uni.toUpperCase() === 'YES')
+{
+  alert('you know me very well');
+}
+else
+{ 
+  alert( 'Its yarmouk university');
+}
+
+let job =prompt('do you know what is my past job experience?');
+if(job.toUpperCase() === 'YES')
+{
+  alert('well done');
+}
+else
+{
+  alert('I was a customer Services');
+}
+
+let hobby =prompt('do you know what is my hobby?');
+if(hobby.toLowerCase === 'yes')
+{
+  alert('that is awsome');
+}
+else if(hobby.toLowerCase() === 'no')
+{
+  alert('I love drawing');
+}
+
+
+
