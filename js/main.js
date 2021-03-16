b66018f74acc1155f5c0bfa1fb6d032033c1aff4
sarah dagamseh <dagamsehsarah@gmail.com>
2021-03-16 21:49:01 +0200

    addQ

--- js/main.js
+++ js/main.js
@@ -53,6 +53,61 @@ else if(hobby.toLowerCase() === 'no' || whatName.toLowerCase() === 'n')
   alert('I love drawing');
 }
 
+let guess =prompt('guess how many years of experience do I have? betwen (1-10)');
+let x=4;
+while (x > 0)
+{
+if (guess >=1 && guess <= 6 ){
+alert('your guess is low !' + x + '  attempt');
+guess = prompt ('guess how many years of experience do I have? betwen (1-10)');
+x--;
+continue;
+}
+ else if (guess >=8 && guess <=10){
+  alert('your guess is high !' + x + '  attempt');
+ guess = prompt ('duess my experience years from 1 -10');
+ x--;
+ continue;}
 
+ else if (guess==7){
+ alert ('you are right');
+ break;
+ 
+}
+else {
+  alert('your guess soo far !' + x + '  attempt');
+ guess = prompt ('guess my experience years from 1 -10');
+ x--;
+ continue;
+}
+}
 
-
+let bank = prompt('write name of any bank you know');
+let x = 5;
+for (let i = 0; i < 6; i++) {
+  switch (bank) {
+  case 'Bank of Jordan':
+    alert('your answer is correct');
+    countScore++;
+    break;
+  case 'Jordan Commercial Bank':
+    alert('your answer is correct');
+    countScore++;
+    break;
+  case 'Jordan Kuwait Bank':
+    alert('your answer is correct');
+    countScore++;
+    break;
+  case 'Al-Itihad Bank':
+    alert('your answer is correct');
+    countScore++;
+    break;
+  default:
+    alert('your answer is wrong, you have ' + x + '  attempts');
+    x--;
+    bank = prompt('write name of any bank you know');
+    continue;
+   }
+   }
+    alert('the correct answer is:  Bank of Jordan, Jordan Commercial Bank, Jordan Kuwait Bank, Al-Itihad Bank ');
+    alert('your final  score is:  ' + countScore);
